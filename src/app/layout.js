45767fb224f0bffa45c68eafcdbc62f35278daaf
katiek78 <kermodekatie@gmail.com>
2025-11-13 21:40:32 +0000

    try to prevent mojibake

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
+        <meta charSet="UTF-8" />
         <link
           href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap"
           rel="stylesheet"
